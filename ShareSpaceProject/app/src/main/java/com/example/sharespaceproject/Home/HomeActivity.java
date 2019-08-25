7f08cb48f3a2f19c856bcff2094454b07f92c28a
everysoon <minsoon1269@naver.com>
2019-08-26 04:41:32 +0900

    seoul

---
+++ ShareSpaceProject/app/src/main/java/com/example/sharespaceproject/Home/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.sharespaceproject.Home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.sharespaceproject.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
