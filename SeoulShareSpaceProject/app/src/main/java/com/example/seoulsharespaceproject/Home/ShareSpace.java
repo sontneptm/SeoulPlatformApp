d53b80adf9e569c7288a2f6ddb8fedddc60c1d49
everysoon <minsoon1269@naver.com>
2019-09-05 01:34:42 +0900

    Android Project

--- SeoulShareSpaceProject/app/src/main/java/com/example/seoulsharespaceproject/Home/ShareSpace.java
+++
@@ -1,6 +0,0 @@
-package com.example.seoulsharespaceproject.Home;
-
-//공유공간 Item을 관리할 클래스
-public class ShareSpace {
-
-}
--- SeoulShareSpaceProject/app/src/main/java/com/example/seoulsharespaceproject/Home/WishListFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.seoulsharespaceproject.Home;
-
-
-import androidx.fragment.app.Fragment;
-
-public class WishListFragment extends Fragment {
-   //찜목록 프래그먼트를 관리하는 자바클래스
-
-}
