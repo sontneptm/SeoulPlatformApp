d7dd3a4512c3e202e4cbe1019b709b051642dcc3
everysoon <minsoon1269@naver.com>
2019-09-05 00:47:59 +0900

    Seoul Project

---
+++ SeoulShareSpaceProject/app/src/main/java/com/example/seoulsharespaceproject/Home/SearchFragment.java
@@ -0,0 +1,23 @@
+package com.example.seoulsharespaceproject.Home;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Spinner;
+
+import com.example.seoulsharespaceproject.R;
+
+import java.util.ArrayList;
+
+public class SearchFragment extends Fragment {
+
+}
---
+++ SeoulShareSpaceProject/app/src/main/java/com/example/seoulsharespaceproject/Home/WishListFragment.java
@@ -0,0 +1,9 @@
+package com.example.seoulsharespaceproject.Home;
+
+
+import androidx.fragment.app.Fragment;
+
+public class WishListFragment extends Fragment {
+   //찜목록 프래그먼트를 관리하는 자바클래스
+
+}
